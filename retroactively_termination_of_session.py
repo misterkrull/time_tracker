22dccdc8a29c4cfdca2024c3515a73ee5ee465e6
Yaroslav Krull (home) <krulll@yandex.ru>
2025-02-13 03:31:58 +0500

    Немного порефакторил, в т.ч. опираясь на предыдущий коммит по дефлоации переменных
    Теперь можно было убрать int у end_subs_datetime_sec, что я и сделал. Вместе с комментом по этому поводу.
    Стало логичнее и понятнее.
    
    Других устаревших комментариев тоже много убрал. Другие немного причесал.
    
    Ещё переменную текстового поля entry заменил на input_field: так больше нравится!

--- retroactively_termination_of_session.py
+++ retroactively_termination_of_session.py
@@ -2,32 +2,21 @@ import time
 import tkinter as tk
 
 from common_functions import sec_to_datetime, datetime_to_sec
-# from time_tracker import ApplicationLogic  
-    # если закольцуется, то можно будет удалить, ибо нужно только ради аннотации
+# from time_tracker import ApplicationLogic
+    # нужно будет раскомментить, когда (если) time_tracker перестанет импортировать gui_layer
 
 def _get_end_current_session_sec(entered_datetime: str, end_subs_datetime_sec: int) -> int:
     try:
-        # TODO подчистить имена переменных в комментах везде в этой функции!!!
-        #  - вот тут немного неясно, что за датавремя
-        #   upd: понял, зачем. Там далее по коду используется self.app.end_current_session
-        #        но я всё-таки решил переделать и не создавать тут self.app.end_current_session
-        #        да, потом в terminate_session придётся обратно из секунд в даты конвертировать 
-        #           лишний раз -- ну и пусть, не велика беда
-        #        зато код становится понятнее
-        #        и не надо дробить функцию datetime_to_sec ради этой копеешной оптимизации
-        # self.app.end_current_session = \
-        #     datetime.strptime(entered_datetime.strip(), '%Y-%m-%d %H:%M:%S')
         end_current_session_sec: int | None = datetime_to_sec(entered_datetime.strip())
     except ValueError:
-        raise ValueError("Вы ввели некорректные дату и время")
-    # self.app.end_current_session_sec = self.app.end_current_session.timestamp()
-    if end_current_session_sec < int(end_subs_datetime_sec):
-        # int() сделали потому, что слева по факту целое число, а справа есть ещё дробная часть,
-        # которая в итоге не отображается - а нам в итоге только целая часть и нужна
-        # в противном случае поведение программы становится немного некорректным
-        raise ValueError("Завершение сессии должно быть позже окончания последней подсессии!")
+        raise ValueError("Вы ввели некорректные дату и время!")
+    
+    if end_current_session_sec < end_subs_datetime_sec:
+        raise ValueError("Завершение сессии должно быть не раньше окончания последней подсессии!")
+    
     if end_current_session_sec >= time.time():
         raise ValueError("Завершение сессии должно быть задним числом, т.е. в прошлом, а не в будущем!")
+    
     return end_current_session_sec
 
 class RetroactivelyTerminationOfSession:
@@ -46,14 +35,14 @@ class RetroactivelyTerminationOfSession:
         # задаём размеры окна
         width = 450
         height = 140
-        # Получаем размеры основного окна - нужно для центрирования нашего окна
+        # задаём расположение окна (используем размеры и расположение родительского окна)
         x = root.winfo_x() + (root.winfo_width() // 2) - width // 2
         y = root.winfo_y() + (root.winfo_height() // 2) - height // 2
 
-        self.dialog_window.geometry(f"{width}x{height}+{x}+{y}")   # указываем размеры и центрируем
-        self.dialog_window.title("Завершить сессию задним числом")
+        self.dialog_window.geometry(f"{width}x{height}+{x}+{y}")  # указываем размеры и расположение
+        self.dialog_window.title("Завершить сессию задним числом")  # указываем название окна
 
-        self._add_widgets()
+        self._add_widgets()  # добавляем все элементы на наше окно
 
         self.dialog_window.bind('<Return>', self._press_enter)
         self.dialog_window.bind('<Escape>', self._on_cancel)
@@ -70,15 +59,15 @@ class RetroactivelyTerminationOfSession:
         label.pack(pady=2)
 
         # добавляем поле для ввода
-        self.entry = tk.Entry(
+        self.input_field = tk.Entry(
             self.dialog_window,
             width=25,
             font=("Segoe UI", 12),
             justify='center'
         )
-        self.entry.pack(pady=3)
-        self.entry.focus_set()
-        self.entry.insert(tk.END, sec_to_datetime(self.app.end_subs_datetime_sec))
+        self.input_field.pack(pady=3)
+        self.input_field.focus_set()
+        self.input_field.insert(tk.END, sec_to_datetime(self.app.end_subs_datetime_sec))
 
         # фрейм для кнопок
         button_frame = tk.Frame(self.dialog_window)
@@ -105,7 +94,7 @@ class RetroactivelyTerminationOfSession:
     def _on_ok(self):
         try:
             self.app.end_current_session_sec = _get_end_current_session_sec(
-                self.entry.get(), self.app.end_subs_datetime_sec
+                self.input_field.get(), self.app.end_subs_datetime_sec
             )
         except ValueError as err:
             tk.messagebox.showerror("Ошибка", str(err))
@@ -118,7 +107,7 @@ class RetroactivelyTerminationOfSession:
         self.dialog_window.destroy()
 
     def _press_enter(self, event=None):
-        if event.widget == self.entry:  # Если фокус на текстовом поле, то нам нужно действие кнопки "ОК"
+        if event.widget == self.input_field:  # Если фокус на текстовом поле, то нам нужно действие кнопки "ОК"
             self.ok_button.config(relief=tk.SUNKEN)  # Имитируем нажатие кнопки "ОК"
             self.ok_button.after(100, lambda: self.ok_button.config(relief=tk.RAISED))  # Имитируем отпускание кнопки "ОК"
             self.ok_button.invoke()  # Вызываем действие кнопки "ОК"
