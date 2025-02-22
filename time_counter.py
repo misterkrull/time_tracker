888d4c99472527c9118e6528c2517b1a93f568ad
Alexey Luzin <Alexey.Luzin@kaspersky.com>
2025-02-22 05:54:42 +0300

    Fix bug with multi loops

--- time_counter.py
+++ time_counter.py
@@ -8,40 +8,40 @@ class TimeCounter:
     def __init__(self, tk_root: tkinter.Tk, on_tick_function: Callable):
         self._tk_root = tk_root
         self._on_tick_function = on_tick_function
-        self._start_time = 0
+        self._start_time = None
         self._is_running = False
+        self._tkinter_task_id = None
 
     def start(self) -> None:
-        self._is_running = True
         self._start_time = time.time()
         self._tick()
 
     def stop(self) -> None:
-        self._is_running = False
+        self._tk_root.after_cancel(self._tkinter_task_id)
+        self._tkinter_task_id = None
 
     def is_running(self):
-        return self._is_running
+        return self._tkinter_task_id is not None
 
     def _tick(self):
         """
         Эта функция вызывает коллбэк и саму себя через секунду, пока счетчик запущен.
         """
-        self._on_tick_function()
-
         print(
             1000 * (time.time() - self._start_time),
             threading.get_ident(),
         )
 
-        if self._is_running:
-            self._tk_root.after(
-                # Интервал времени - секунда минус отклонение от реальной секунды.
-                ms=int(1000 * (1 - (time.time() - self._start_time) % 1)),
-                func=self._tick,
-            )
+        self._on_tick_function()
 
-            # Альтернативный вариант с отдельным потоком:
-            # threading.Timer(
-            #     <интервал в секундах>,
-            #     self._tick
-            # ).start()
+        self._tkinter_task_id = self._tk_root.after(
+            # Интервал времени - секунда минус отклонение от реальной секунды.
+            ms=int(1000 * (1 - (time.time() - self._start_time) % 1)),
+            func=self._tick,
+        )
+
+        # Альтернативный вариант с отдельным потоком:
+        # threading.Timer(
+        #     <интервал в секундах>,
+        #     self._tick
+        # ).start()
