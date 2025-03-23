21c47dc4da7a0aaaa0a93d4aa45480d398a2fcff
Yaroslav Krull (home) <krulll@yandex.ru>
2025-03-23 05:20:25 +0500

    Сменил количество таймеров с 5 на 3

--- gui/gui_constants.py
+++ gui/gui_constants.py
@@ -1,7 +1,7 @@
-TIMER_FRAME_COUNT = 5
+TIMER_FRAME_COUNT = 3
 
 # для 3 таймеров (и для прода) нужно 678, для пяти таймеров хорошо 1070
-MAIN_WINDOW_X = 1070
+MAIN_WINDOW_X = 678
 MAIN_WINDOW_Y = 250
 
 BACKGROUND_COLOR = "SystemButtonFace"  # дефолтный виндовый цвет окон, типа серый такой
